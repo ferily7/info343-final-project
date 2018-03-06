52ed031cc7327324cbc3458c9fd08d173506a87b
Laura Freeman <neonlight003@gmail.com>
2018-03-06 09:48:51 -0800

    add logout

--- src/components/sidebar-component/Sidebar.js
+++ src/components/sidebar-component/Sidebar.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import firebase from "firebase";
 
 class Sidebar extends Component {
+    handleSignOut() {
+        this.setState({ errorMessage: null });
+        firebase
+          .auth()
+          .signOut()
+          .catch(err => {
+            console.log(err);
+            this.setState({ errorMessage: err.message });
+          });
+      }
     render() {
         return (
             <div>
+                <button type="button" onClick={() => this.handleSignOut()}>Log Out</button>
                 Sidebar here
             </div>
         )
