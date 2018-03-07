c1a9ceb43ee862bd38e5a5316b5bd712fceb1789
kwokwilliam <wkwok16@uw.edu>
2018-03-07 02:10:52 -0800

    Made a small test change

--- src/App.js
+++ src/App.js
@@ -11,89 +11,89 @@ import LandingPage from "./components/pages/LandingPage";
 import UserHome from "./components/pages/UserHome";
 
 const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: '#f75830',
-  },
-  appBar: {
-    height: 56
-  }
+    palette: {
+        primary1Color: '#f75830',
+    },
+    appBar: {
+        height: 56
+    }
 });
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTrip: "", // Keep track of selected trip for rendering pages
-      firebaseUser: null // Keep track of firebase user
-    };
-    this.changeSelectedTrip = this.changeSelectedTrip.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTrip: "", // Keep track of selected trip for rendering pages
+            firebaseUser: null // Keep track of firebase user
+        };
+        this.changeSelectedTrip = this.changeSelectedTrip.bind(this);
+    }
 
-  // Keep track of authentication state, to see if user is logged in
-  componentDidMount() {
-    this.stopWatchingAuth = firebase.auth().onAuthStateChanged(firebaseUser => {
-      firebaseUser
-        ? this.setState({ firebaseUser: firebaseUser })
-        : this.setState({ firebaseUser: null });
-    });
-  }
+    // Keep track of authentication state, to see if user is logged in
+    componentDidMount() {
+        this.stopWatchingAuth = firebase.auth().onAuthStateChanged(firebaseUser => {
+            firebaseUser
+                ? this.setState({ firebaseUser: firebaseUser })
+                : this.setState({ firebaseUser: null });
+        });
+    }
 
-  // Stop watching authentication if app unmounts
-  componentWillUnmount() {
-    this.stopWatchingAuth();
-  }
+    // Stop watching authentication if app unmounts
+    componentWillUnmount() {
+        this.stopWatchingAuth();
+    }
 
-  // changeSelectedTrip will change the selected trip
-  // To be used with the sidebar when clicking on a different trip
-  //      This means we pass it down to UserHome then to the Sidebar
-  // Prerequisite: trips must have unique names or IDs, it'd be way easier to have them have unique names than IDs, in personal experience
-  // @param   trip    trip is a type string, expected either name or ID depending on what we choose
-  changeSelectedTrip(trip) {
-    this.setState({ selectedTrip: trip });
-  }
+    // changeSelectedTrip will change the selected trip
+    // To be used with the sidebar when clicking on a different trip
+    //      This means we pass it down to UserHome then to the Sidebar
+    // Prerequisite: trips must have unique names or IDs, it'd be way easier to have them have unique names than IDs, in personal experience
+    // @param   trip    trip is a type string, expected either name or ID depending on what we choose
+    changeSelectedTrip(trip) {
+        this.setState({ selectedTrip: trip });
+    }
 
-  render() {
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <Router>
-          <div className="app">
-            {/* <Header />
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Router>
+                    <div className="app">
+                        {/* <Header />
                     <Route exact path="/" component={Homepage} />
                     <Route exact path="/dashboard" component={Dashboard} /> */}
 
-            {/* William Kwok's temporary code edits start down here, uncomment the above and comment below if other things need to be tested.
+                        {/* William Kwok's temporary code edits start down here, uncomment the above and comment below if other things need to be tested.
                         Currently will cause compilation errors because of nonexistant objects*/}
-            <Header user={this.state.firebaseUser} />
-            <Route
-              exact
-              path="/"
-              render={routerProps => (
-                <LandingPage
-                  {...routerProps}
-                  firebaseUser={this.state.firebaseUser}
-                />
-              )}
-            />
-            <Route
-              path="/userhome"
-              render={routerProps => (
-                <UserHome
-                  firebaseUser={this.state.firebaseUser}
-                  selectedTrip={this.state.selectedTrip}
-                  changeSelectedTrip={this.changeSelectedTrip}
-                />
-              )}
-            />
-            <Route
-            path="/login"
-            render={routerProps => <Login firebaseUser={this.state.firebaseUser} />}
-          />
-          </div>
-        </Router>
-      </MuiThemeProvider>
-    );
-  }
+                        <Header user={this.state.firebaseUser} />
+                        <Route
+                            exact
+                            path="/"
+                            render={routerProps => (
+                                <LandingPage
+                                    {...routerProps}
+                                    firebaseUser={this.state.firebaseUser}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/userhome"
+                            render={routerProps => (
+                                <UserHome
+                                    firebaseUser={this.state.firebaseUser}
+                                    selectedTrip={this.state.selectedTrip}
+                                    changeSelectedTrip={this.changeSelectedTrip}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/login"
+                            render={routerProps => <Login firebaseUser={this.state.firebaseUser} />}
+                        />
+                    </div>
+                </Router>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
--- src/components/sidebar-component/Sidebar.js
+++ src/components/sidebar-component/Sidebar.js
@@ -27,7 +27,6 @@ class Sidebar extends Component {
         this.mounted = true;
         if (this.props.firebaseUser) {
             this.orgReference = firebase.database().ref(`${this.props.firebaseUser.uid}/trips`);
-            console.log(this.orgReference);
             this.orgReference.on('value', (snapshot) => {
                 if (this.mounted) {
                     this.setState({ orgReference: snapshot.val() });
