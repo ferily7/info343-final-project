d3415780bbb40787cf072f8dadf191717cceb476
mockingod1841 <mockingod1841@gmail.com>
2018-03-07 12:59:08 -0800

    Commented code and also renamed a variable

--- src/components/content-components/Itinerary.js
+++ src/components/content-components/Itinerary.js
@@ -5,28 +5,42 @@ class Itinerary extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            orgReference: null
+            dataRef: null,
+            cost: 0,
+            eventEnd: 0,
+            eventStart: 0,
+            eventName: '',
+            location: '',
+            reservation: null,
+            type: '',
+            errorMessage: ''
         }
     }
+
+    // Component will receive the correct selected trip, update the reference to the trip when this is done
     componentWillReceiveProps(inProp) {
-        this.orgReference = firebase.database().ref(`${inProp.firebaseUser.uid}/trips/${inProp.selectedTrip}`);
-        this.orgReference.on('value', (snapshot) => {
+        this.dataRef = firebase.database().ref(`${inProp.firebaseUser.uid}/trips/${inProp.selectedTrip}`);
+        this.dataRef.on('value', (snapshot) => {
             if (this.mounted) {
-                this.setState({ orgReference: snapshot.val() });
+                this.setState({ dataRef: snapshot.val() });
             }
         })
     }
+
+    // Grab what it can when component mounts, need this for when switching between tabs.
     componentDidMount() {
         this.mounted = true;
         if (this.props.firebaseUser) {
-        this.orgReference = firebase.database().ref(`${this.props.firebaseUser.uid}/trips/${this.props.selectedTrip}`);
-            this.orgReference.on('value', (snapshot) => {
+            this.dataRef = firebase.database().ref(`${this.props.firebaseUser.uid}/trips/${this.props.selectedTrip}`);
+            this.dataRef.on('value', (snapshot) => {
                 if (this.mounted) {
-                    this.setState({ orgReference: snapshot.val() });
+                    this.setState({ dataRef: snapshot.val() });
                 }
             })
         }
     }
+
+    // Set unmount state so doesn't update when not mounted anymore
     componentWillUnmount() {
         this.mounted = false;
     }
@@ -34,6 +48,36 @@ class Itinerary extends Component {
     // Load calendar based on existing events from state
     // Click events for updating calendar events
     // Add event to database based on inputs
+    addEvent() {
+        if (this.state.eventEnd === 0 || this.state.eventStart === 0) {
+            this.setState({ errorMessage: "Event dates unspecified" });
+        } else if (this.state.type === '') {
+            this.setState({ errorMessage: "Event type not specified" });
+        } else if (this.state.eventName === '') {
+            this.setState({ errorMessage: "Event unnamed" });
+        } else {
+            let newEvent = {
+                cost: this.state.cost,
+                eventEnd: this.state.eventEnd,
+                eventStart: this.state.eventStart,
+                eventName: this.state.eventName,
+                location: this.state.location,
+                reservation: this.state.reservation,
+                type: this.state.type
+            }
+            this.dataRef.child("events").push(newEvent);
+            this.setState({
+                cost: 0,
+                eventEnd: 0,
+                eventStart: 0,
+                eventName: '',
+                location: '',
+                reservation: null,
+                type: '',
+                errorMessage: ''
+            });
+        }
+    }
 
 
 
@@ -46,8 +90,8 @@ class Itinerary extends Component {
                         Select a trip to begin
                     </div>
                 }
-                {this.props.selectedTrip !== "" && this.state.orgReference &&
-                    <div>{this.state.orgReference.tripName}{console.log(this.state.orgReference)}</div>
+                {this.props.selectedTrip !== "" && this.state.dataRef &&
+                    <div>{this.state.dataRef.tripName}{console.log(this.state.dataRef)}</div>
                 }
             </div>
 
--- src/components/content-components/Reservations.js
+++ src/components/content-components/Reservations.js
@@ -5,28 +5,33 @@ class Reservations extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            orgReference: null
+            dataRef: null
         }
     }
+    // Component will receive the correct selected trip, update the reference to the trip when this is done
     componentWillReceiveProps(inProp) {
-        this.orgReference = firebase.database().ref(`${inProp.firebaseUser.uid}/trips/${inProp.selectedTrip}`);
-        this.orgReference.on('value', (snapshot) => {
+        this.dataRef = firebase.database().ref(`${inProp.firebaseUser.uid}/trips/${inProp.selectedTrip}`);
+        this.dataRef.on('value', (snapshot) => {
             if (this.mounted) {
-                this.setState({ orgReference: snapshot.val() });
+                this.setState({ dataRef: snapshot.val() });
             }
         })
     }
+
+    // Grab what it can when component mounts, need this for when switching between tabs.
     componentDidMount() {
         this.mounted = true;
         if (this.props.firebaseUser) {
-            this.orgReference = firebase.database().ref(`${this.props.firebaseUser.uid}/trips/${this.props.selectedTrip}`);
-            this.orgReference.on('value', (snapshot) => {
+            this.dataRef = firebase.database().ref(`${this.props.firebaseUser.uid}/trips/${this.props.selectedTrip}`);
+            this.dataRef.on('value', (snapshot) => {
                 if (this.mounted) {
-                    this.setState({ orgReference: snapshot.val() });
+                    this.setState({ dataRef: snapshot.val() });
                 }
             })
         }
     }
+
+    // Set unmount state so doesn't update when not mounted anymore
     componentWillUnmount() {
         this.mounted = false;
     }
@@ -39,8 +44,8 @@ class Reservations extends Component {
                         Select a trip to begin
                     </div>
                 }
-                {this.props.selectedTrip !== "" && this.state.orgReference &&
-                    <div>{this.state.orgReference.tripName}{console.log(this.state.orgReference)}</div>
+                {this.props.selectedTrip !== "" && this.state.dataRef &&
+                    <div>{this.state.dataRef.tripName}{console.log(this.state.dataRef)}</div>
                 }
             </div>
 
