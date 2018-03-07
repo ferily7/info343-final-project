5fa5f75b582976fba6528421c0599bf9e49da370
kwokwilliam <wkwok16@uw.edu>
2018-03-07 02:09:47 -0800

    Made sidebar work

--- src/components/pages/UserHome.js
+++ src/components/pages/UserHome.js
@@ -5,40 +5,42 @@ import Navbar from "../navbar-component/Navbar";
 import UserHomeContent from "./UserHomeContent";
 
 class UserHome extends Component {
-  render() {
-    return (
-      <Grid fluid className="translate-up no-padding no-margin">
-        <Row className="tabs-row no-padding no-margin">
-          <Col
-            className="no-padding"
-            smOffset={4}
-            mdOffset={3}
-            lgOffset={2}
-            xs={12}
-            sm={8}
-            md={9}
-            lg={10}
-          >
-            <Navbar />
-          </Col>
-        </Row>
-        <Row className="no-padding no-margin">
-          <Col className="no-padding" xs={12} sm={4} md={3} lg={2}>
-            <Sidebar
-              firebaseUser={this.props.firebaseUser}
-              selectedTrip={this.props.selectedTrip}
-            />
-          </Col>
-          <Col className="no-padding" xs={12} sm={8} md={9} lg={10}>
-            <UserHomeContent
-              firebaseUser={this.props.firebaseUser}
-              selectedTrip={this.props.selectedTrip}
-            />
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <Grid fluid className="translate-up no-padding no-margin">
+                <Row className="tabs-row no-padding no-margin">
+                    <Col
+                        className="no-padding"
+                        smOffset={4}
+                        mdOffset={3}
+                        lgOffset={2}
+                        xs={12}
+                        sm={8}
+                        md={9}
+                        lg={10}
+                    >
+                        <Navbar />
+                    </Col>
+                </Row>
+                <Row className="no-padding no-margin">
+                    <Col className="no-padding" xs={12} sm={4} md={3} lg={2}>
+                        <Sidebar
+                            firebaseUser={this.props.firebaseUser}
+                            selectedTrip={this.props.selectedTrip}
+                            changeSelectedTrip={this.props.changeSelectedTrip}
+                        />
+                    </Col>
+                    <Col className="no-padding" xs={12} sm={8} md={9} lg={10}>
+                        <UserHomeContent
+                            firebaseUser={this.props.firebaseUser}
+                            selectedTrip={this.props.selectedTrip}
+
+                        />
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 export default UserHome;
--- src/components/sidebar-component/Sidebar.js
+++ src/components/sidebar-component/Sidebar.js
@@ -7,48 +7,70 @@ import RaisedButton from "material-ui/RaisedButton";
 import { List, ListItem } from "material-ui/List";
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      errorMessage: ""
-    };
-  }
-  handleSignOut() {
-    firebase
-      .auth()
-      .signOut()
-      .catch(err => {
-        console.log(err);
-        this.setState({ errorMessage: err.message });
-      });
-  }
-  render() {
-    return (
-      <div>
-        {this.props.firebaseUser ? undefined : <Redirect to="/" />}
-        <div className="sidebar">
-          <List className="trip-list">
-            <ListItem className="trip-list-item" primaryText="Trip 1" />
-            <ListItem className="trip-list-item" primaryText="Trip 2" />
-            <ListItem className="trip-list-item" primaryText="Trip 3" />
-            <ListItem className="trip-list-item" primaryText="Trip 4" />
-            <ListItem
-              id="new-trip"
-              className="trip-list-item"
-              primaryText="+ New"
-            />
-          </List>
+    constructor(props) {
+        super(props);
+        this.state = {
+            errorMessage: "",
+            orgReference: null
+        };
+    }
+    handleSignOut() {
+        firebase
+            .auth()
+            .signOut()
+            .catch(err => {
+                console.log(err);
+                this.setState({ errorMessage: err.message });
+            });
+    }
+    componentDidMount() {
+        this.mounted = true;
+        if (this.props.firebaseUser) {
+            this.orgReference = firebase.database().ref(`${this.props.firebaseUser.uid}/trips`);
+            console.log(this.orgReference);
+            this.orgReference.on('value', (snapshot) => {
+                if (this.mounted) {
+                    this.setState({ orgReference: snapshot.val() });
+                }
+            })
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.props.firebaseUser ? undefined : <Redirect to="/" />}
+                <div className="sidebar">
+                    <List className="trip-list">
+                        {
+                            this.state.orgReference &&
+                            Object.keys(this.state.orgReference).map((d) => {
+                                return <ListItem className="trip-list-item"
+                                    key={d}
+                                    primaryText={this.state.orgReference[d].tripName}
+                                    onClick={() => this.props.changeSelectedTrip(this.state.orgReference[d].tripName)} />
+                            })
+                        }
+                        {/* <ListItem className="trip-list-item" primaryText="Trip 1" />
+                        <ListItem className="trip-list-item" primaryText="Trip 2" />
+                        <ListItem className="trip-list-item" primaryText="Trip 3" />
+                        <ListItem className="trip-list-item" primaryText="Trip 4" /> */}
+                        <ListItem
+                            id="new-trip"
+                            className="trip-list-item"
+                            primaryText="+ New"
+                        />
+                    </List>
 
-          <RaisedButton
-            className="signout-button"
-            label="Sign Out"
-            primary={true}
-            onClick={() => this.handleSignOut()}
-          />
-        </div>
-      </div>
-    );
-  }
+                    <RaisedButton
+                        className="signout-button"
+                        label="Sign Out"
+                        primary={true}
+                        onClick={() => this.handleSignOut()}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Sidebar;
