d8349946c83a12270d2940aa0c67b55427e7347c
Farrah Lee <farrahyglee@yahoo.com>
2018-03-08 11:30:07 -0800

    outlined the page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,19 +6,26 @@ class About extends Component {
     render() {
         return (
             <div>
-<Grid fluid>
-          <Row>
-            <Col smOffset={1} xs={12} sm={10}>
-            <div className="contain-sm">
-              <h1 className="page-header">About</h1>
-              <p>
-                  This is the about/attributions page.
-                  </p>
-                  </div>
-              </Col>
-              </Row>
-              </Grid>
-                </div>
+                <Grid fluid>
+                    <Row>
+                        <Col smOffset={1} xs={12} sm={10}>
+                        <div className="contain-sm">
+                            <h1 className="page-header">About</h1>
+
+                            <h2>Itinerary</h2>
+                            <p>Information about how the itinerary works goes here</p>
+                                
+                            <h2>Budgeting</h2>
+                            <p>Information about how budgeting works goes here</p>
+
+                            <h2>Reservations</h2>
+                            <p>Information about how reservations work goes here</p>
+
+                        </div>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         );
     }
 }
